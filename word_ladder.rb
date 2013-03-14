8afaac86a5f4b8ccb5b07533a899fb388275b8fb
Alex Kroman <alex@newrelic.com>
2013-03-14 07:53:45 -0700

    Blah, tabs to spaces sublime

--- word_ladder.rb
+++ word_ladder.rb
@@ -3,13 +3,13 @@ def words
 end
 
 def ladder(starting_word)
-	output = ''
+  output = ''
   words.each do |word|
-  		distance = 0
-  		word.chomp.chars.each_with_index do |character, i|
-  			distance += 1 unless character == starting_word[i]
-  		end
-  		output << word if distance == 1
+      distance = 0
+      word.chomp.chars.each_with_index do |character, i|
+        distance += 1 unless character == starting_word[i]
+      end
+      output << word if distance == 1
   end
   output
 end
